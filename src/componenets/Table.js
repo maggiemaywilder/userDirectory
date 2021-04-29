94731d3e9515bee9d4768aeb9874566dedafe512
Maggie may <maggiemaywilder@gmail.com>
2021-04-29 17:55:32 -0500

    first stage table rendering

--- src/componenets/Table.js
+++ src/componenets/Table.js
@@ -1,35 +1,67 @@
 import React, { Component } from 'react';
 import API from '../utils/API';
 
-var employeeList;
 
 class Table extends Component {
-    state = {
-        data: []
+
+    constructor(props) {
+        super(props);
+        this.state = [];
     }
-
-
-    
 
     componentDidMount() {
         this.fetchEmployees();
-    }
-
-    fetchEmployees = async () => {
-        let data = await API.getEmployees();
-        employeeList = data.data.results;
-        console.log('line 20', employeeList);
-        this.setState(employeeList);
     };
 
+    // componentWillUnmount() {
+
+    // }
+
+    fetchEmployees() {
+        API.getEmployees()
+            .then(result => this.setState([result.data.results]))
+            .catch(err => console.log(err));
+    };
+
+    renderTableData() {
+        console.log(this.state[0]);
+
+        // return this.state.map((each, index) => {
+        //     return (
+        //       <tr key={index}>
+        //           <td>{each.state.picture.thumbnail}</td>
+        //           <td>`${each.state.name.first} ${each.state.name.last}`</td>
+        //           <td>{each.state.dob}</td>
+        //           <td>{each.state.phone}</td>
+        //           <td>{each.state.email}</td>
+        //       </tr>
+        // )
+        // })
+
+    }
 
     render() {
         return (
             <div>
-                <h1>Hello something and thanks for all the fish</h1>
+                <h1>Hello World and thanks for all the fish</h1>
+
+                <table className="table table-striped">
+                    <thead className="thead-dark" >
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Birthday</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
             </div>
         )
     }
 }
 
-export default Table
+export default Table;
